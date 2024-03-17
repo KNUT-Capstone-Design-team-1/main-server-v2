5615ef734bf9c24dc6b6e6182c30a9945a7285c7
YongSihun <tlgns1537@naver.com>
2024-03-17 17:41:42 +0900

    #18 run.sh 파라미터 분기 도입
    - typescript 빌드 도입 (STAND-ALONE 모드에서는 git pull 사용을 위해 src 파일을 제거하지 않음)

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,5 @@
 FROM node:16.13.2-alpine
 
-# Arguments
 ARG MAIN_SERVER_PORT
 ARG DB_URL
 ARG NODE_ENV
@@ -10,7 +9,6 @@ ARG DL_SERVER_ADDR
 ARG DL_SERVER_PORT
 ARG DL_SERVER_IMG_RECOG_PATH
 
-# 환경변수 선언
 ENV MAIN_SERVER_PORT=$MAIN_SERVER_PORT
 ENV DB_URL=$DB_URL
 ENV NODE_ENV=$NODE_ENV
@@ -20,17 +18,19 @@ ENV DL_SERVER_ADDR=$DL_SERVER_ADDR
 ENV DL_SERVER_PORT=$DL_SERVER_PORT
 ENV DL_SERVER_IMG_RECOG_PATH=$DL_SERVER_IMG_RECOG_PATH
 
-# 컨테이너 내 디렉터리로 파일 복사
 COPY . /usr/local/wip-main-server-v2 
 
-# 작업 디렉터리 이동
+WORKDIR /usr/local/wip-main-server-v2
+
+RUN npm install
+RUN npm install -g typescript
+RUN tsc --build
+RUN mv -f ./build/src ./src
+RUN mv -f ./build/doc ./doc
+RUN mv -f ./build/res ./res
+
 WORKDIR /usr/local/wip-main-server-v2/src
 
-# node 모듈 설치
-RUN npm install
-
-# 포트
 EXPOSE $MAIN_SERVER_PORT
 
-# 실행
 ENTRYPOINT ["node", "app.js"]
--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+build_mode=$1
+
 # 서버 내 .env 파일로 부터 환경변수 정의
 echo "---- set environment values ----"
 unamestr=$(uname)
@@ -12,23 +14,29 @@ elif [ "$unamestr"='FreeBSD' ] || [ "$unamestr"='Darwin' ]; then
 fi
 echo "---- OK ----"
 
-# 도커 이미지 빌드
-echo "---- container image build ----"
-build_cmd="docker build . -t wip-main-server-v2"
-while read line; do
-   arg_temp=$(echo $line | cut -f 1 -d'=')
-   build_cmd+=" --build-arg $arg_temp=$(eval echo '$'$arg_temp)"
-done < .env
+if [ $build_mode='STAND-ALONE' ]; then
+  tsc --build
+  npm install
+  node ./build/src/app.js
+elif [ $build_mode='SINGLE-CONTAINER' ]; then
+  # 도커 이미지 빌드
+  echo "---- container image build ----"
+  build_cmd="docker build . -t wip-main-server-v2"
+  while read line; do
+    arg_temp=$(echo $line | cut -f 1 -d'=')
+    build_cmd+=" --build-arg $arg_temp=$(eval echo '$'$arg_temp)"
+  done < .env
 
-$(echo $build_cmd)
-echo "---- OK ----"
+  $(echo $build_cmd)
+  echo "---- OK ----"
 
-# 컨테이너 종료. 실행중인 컨테이너가 있으면 강제로 제거
-echo "---- remove previous container ----"
-docker container rm -f wip-main-server-v2
-echo "---- OK ----"
+  # 컨테이너 종료. 실행중인 컨테이너가 있으면 강제로 제거
+  echo "---- remove previous container ----"
+  docker container rm -f wip-main-server-v2
+  echo "---- OK ----"
 
-# 컨테이너 실행
-echo "---- run container ----"
-docker run -d --name wip-main-server-v2 wip-main-server-v2
-echo "---- OK ----"
+  # 컨테이너 실행
+  echo "---- run container ----"
+  docker run -d --name wip-main-server-v2 wip-main-server-v2
+  echo "---- OK ----"
+fi
