c79ce6242d94a7d20f0878c984a19893d575cb24
YongSihun <tlgns1537@naver.com>
2024-03-23 16:56:04 +0900

    bug: run.sh에서 절대 경로로 볼륨 매핑
    #20

--- run.sh
+++ run.sh
@@ -42,7 +42,9 @@ elif [ $1 = "SINGLE-CONTAINER" ]; then
   echo "---- OK ----"
 
   echo "---- Run container ----"
-  docker run -d --name wip-main-server-v2 wip-main-server-v2 --add-host host.docker.internal:host-gateway -v ./logs
+  docker run -d --name wip-main-server-v2 wip-main-server-v2 \
+    --add-host host.docker.internal:host-gateway \
+    -v /must_be_madness/what_is_pill/wip-main-server-v2/logs:/usr/local/wip-main-server-v2/logs
   echo "---- OK ----"
 
   echo "---- Remove previous image ----"
