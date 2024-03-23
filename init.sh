2a2be4a045f7bd059594b093a2b49946eaf36c8a
YongSihun <tlgns1537@naver.com>
2024-03-23 15:40:14 +0900

    docs: init.sh 주석 제거

--- init.sh
+++ init.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 sudo apt update -y
 
-# node install
 sudo apt install -y nvm
 source ~/.bashrc
 nvm install 16.20.2
 sudo ln -sf /home/ubuntu/.nvm/versions/node/v16.20.2/bin/node /usr/local/bin/node
 sudo ln -sf /home/ubuntu/.nvm/versions/node/v16.20.2/bin/npm /usr/local/bin/npm
 
-# service daemon setting
 sudo cp /must_be_madness/what_is_pill/wip-deep-learning-server-v2/system/wip-main-server-v2.service \
      /etc/systemd/system/wip-main-server-v2.service
 sudo systemctl enable wip-main-server-v2
