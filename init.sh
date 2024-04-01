338430d5401d432919784b1941b21e296c5c7d35
YongSihun <tlgns1537@naver.com>
2024-04-01 21:55:11 +0900

    chore: init.sh 유연화
    #22

--- init.sh
+++ init.sh
@@ -4,8 +4,9 @@ sudo apt update -y
 sudo apt install -y nvm
 source ~/.bashrc
 nvm install 20.12.2
-sudo ln -sf /home/ubuntu/.nvm/versions/node/v16.20.2/bin/node /usr/local/bin/node
-sudo ln -sf /home/ubuntu/.nvm/versions/node/v16.20.2/bin/npm /usr/local/bin/npm
+sudo ln -sf "$NVM_DIR/versions/node/$(nvm version)/bin/node" "/usr/local/bin/node"
+sudo ln -sf "$NVM_DIR/versions/node/$(nvm version)/bin/npm" "/usr/local/bin/npm"
+sudo ln -sf "$NVM_DIR/versions/node/$(nvm version)/bin/npx" "/usr/local/bin/npx"
 
 sudo cp /must_be_madness/what_is_pill/wip-deep-learning-server-v2/system/wip-main-server-v2.service \
      /etc/systemd/system/wip-main-server-v2.service
